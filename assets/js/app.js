5c3864be20b737a0110e57f6801d2933bdc82c6f
Samantha Quiñones <squinones@politico.com>
2014-09-22 13:32:33 -0400

    Added CNAME and events

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,5 +12,12 @@ $( document ).ready(function() {
 	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
 	
 	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
-
+	
+	var feed = "https://www.google.com/calendar/feeds/0fe90veo9ms9bt5i5a2blce8dc%40group.calendar.google.com/public/full?alt=json";
+	$.getJSON(feed, function (data) {
+		entries = data.feed.entry;
+		for (var i=0; i<entries.length;i++) {
+			console.log(entries[i]);
+		}
+	});
 });
