7d3b3f983f735e00c79991d91e6da6fadd2dce44
Samantha Quiñones <squinones@politico.com>
2014-09-24 00:06:47 -0400

    Removed old calendar stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,18 +6,10 @@ $( document ).ready(function() {
 	/* Secondary contact links */
 	var scontacts = $('#contact-list-secondary');
 	var contact_list = $('#contact-list');
-	
+
 	scontacts.hide();
-	
+
 	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
-	
+
 	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
-	
-	var feed = "https://www.google.com/calendar/feeds/0fe90veo9ms9bt5i5a2blce8dc%40group.calendar.google.com/public/full?alt=json";
-	$.getJSON(feed, function (data) {
-		entries = data.feed.entry;
-		for (var i=0; i<entries.length;i++) {
-			console.log(entries[i]);
-		}
-	});
 });
